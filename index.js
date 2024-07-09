5b9b603ceb33ab0cd5a637f62dcb4e0a56d80c32
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-09 12:34:32 +0530

    Backend uploaded

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+app.use("/api/user/", UserRoutes);
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGO_DB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
