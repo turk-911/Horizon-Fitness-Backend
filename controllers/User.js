e29b2c66d62a0a950ff404983c92e89071ae4786
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-09 15:46:10 +0530

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -186,7 +186,7 @@ export const getUserDashboard = async (req, res, next) => {
 
 export const getWorkoutsByDate = async (req, res, next) => {
   try {
-    const userId = req.user?.id;
+    const userId = req.user?._id;
     const user = await User.findById(userId);
     let date = req.query.date ? new Date(req.query.date) : new Date();
     if (!user) {
