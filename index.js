fc62d41eeafb45b37e18b714909f8227ce1336bf
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-09 12:58:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: ["https://horizon-fitness.netlify.app", "http://localhost:5173"],
-  methods: ["POST", "GET"],
-  credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
